52d3c55e02d5362e1917b0b72bb1c8b3fc39dfa3
Maxsos133 <max.pavlenko133@gmail.com>
2023-05-09 10:46:19 -0500

    finish lesson

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log('hello world')
+
+// console.log( 3 ** 2)
+
+let firstName = `Maxim`
+let lastName = `Pavlenko`
+let fullName = firstName + ` ` + lastName
+let age = 21
+let enjoysCoding = true
+let enjoysDay1Installfest = false
+
+console.log(typeof enjoysCoding)
